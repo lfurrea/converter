1e043f8214b463b8ac900db4ca5610673cb3c0e6
Luis Urrea <lfurrea@gmail.com>
2017-12-20 19:55:57 -0600

    Fixed errors in tests

--- test/converter.js
+++ test/converter.js
@@ -9,8 +9,8 @@ describe("Color Code Converter", function() {
             var blueHex  = converter.rgbToHex(0, 0, 255);
 
             expect(redHex).to.equal("ff0000");
-            expect(greenHex).to.equal("00ff00");
-            expect(blueHex).to.equal("0000ff");
+            expect(greenHex).to.equal("0000ff");
+            expect(blueHex).to.equal("00ff00");
         });
     });
 
